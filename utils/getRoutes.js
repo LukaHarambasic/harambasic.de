8ba70c9181918e7a167e80ffb9085f037bdf3b12
Luka Harambasic <luka@harambasic.de>
2020-11-13 21:15:35 +0100

    sitemap - closes 10

--- utils/getRoutes.js
+++ utils/getRoutes.js
@@ -1,5 +1,7 @@
 export default async () => {
   const { $content } = require('@nuxt/content')
   const files = await $content({ deep: true }).only(['path']).fetch()
-  return files.map((file) => (file.path === '/index' ? '/' : file.path))
+  return files
+    .map((file) => (file.path === '/index' ? '/' : file.path))
+    .filter((file) => !file.path.includes('/projects/')) // exclude all projects as they don't have a details page
 }
