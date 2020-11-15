4bf484d7cb2550c5c95ee26aa17f283c470d0270
Luka Harambasic <luka@harambasic.de>
2020-11-15 13:18:01 +0100

    home to nuxt content

---
+++ content/home.md
@@ -0,0 +1,3 @@
+## Hi! I'm Luka 🧔🏻
+
+You'll get some insights in [projects](/projects) I did. But I'll also share some thoughts, knowledge and experiences in my [blog](/posts). If you want to see my CV you can checkout [LinkedIn](https://www.linkedin.com/in/harambasic/). And if you still want to know more about me you can also write me an [email](mailto:hi@harambasic.de).
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,18 @@
 <template>
-  <section class="center-center nuxt-content">
-    <div class="content">
-      <h2>Hi! I'm Luka 🧔🏻</h2>
-      <p>
-        You'll get some insights in
-        <nuxt-link to="/projects">Projects</nuxt-link> I did. But I'll also
-        share some thoughts, knowledge and experiences in my
-        <nuxt-link to="/posts">Blog</nuxt-link>. If you want to see my CV you
-        can checkout
-        <a href="https://www.linkedin.com/in/harambasic/">LinkedIn</a>. And if
-        you still want to know more about me you can also write me a
-        <a href="mailto:hi@harambasic.de">Mail</a>.
-      </p>
-    </div>
+  <section class="center-center">
+    <nuxt-content class="markdown" :document="page" />
   </section>
 </template>
 
 <script>
 export default {
+  async asyncData({ $content, params }) {
+    const page = await $content('home').fetch()
+    console.log(page)
+    return {
+      page,
+    }
+  },
   head() {
     return {
       title: 'Luka Harambasic',
