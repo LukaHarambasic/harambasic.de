b8768c07947d60ad19c891e7826d4adc1900935f
Luka Harambasic <luka@harambasic.de>
2020-12-20 00:55:41 +0100

    adding local typefaces: open sans & jet brians mono

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['~assets/style/main.sass'],
+  css: ['@assets/style/main.sass', '@assets/style/fontfaces.css'],
   /*
    ** Load Variables
    */
