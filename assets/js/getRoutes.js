cbda5f1b403bba78faa50da92f4211caa06d5b6e
Luka Harambasic <luka@harambasic.de>
2021-01-24 12:33:17 +0100

    adding twitter link

--- assets/js/getRoutes.js
+++ assets/js/getRoutes.js
@@ -1,9 +1,11 @@
 export default async () => {
   const { $content } = require('@nuxt/content')
   const files = await $content({ deep: true }).only(['path']).fetch()
+  const regex = new RegExp('/\\/cv\\/.*\\//gm')
   return files
     .map((file) => {
       if (file.path === '/home') return
+      if (file.path.test(regex)) return
       if (file.path === '/index') return '/'
       return file.path
     })
