250b77693eb6eb77d17764f733b737e3cec78800
Luka Harambasic <luka@harambasic.de>
2021-01-24 13:11:06 +0100

    fixed route generation

--- assets/js/getRoutes.js
+++ assets/js/getRoutes.js
@@ -1,14 +1,14 @@
 export default async () => {
   const { $content } = require('@nuxt/content')
   const files = await $content({ deep: true }).only(['path']).fetch()
-  const regex = new RegExp('/\\/cv\\/.*\\//gm')
+  // test for e.g. /cv/settings - as they were previously generated
+  const regex = new RegExp('\\/cv\\/.+')
   return files
     .map((file) => {
-      if (file.path === '/home') return
-      if (file.path.test(regex)) return
       if (file.path === '/index') return '/'
       return file.path
     })
-    .filter((path) => path !== undefined)
+    .filter((path) => path !== '/home')
+    .filter((path) => !regex.test(path))
     .filter((path) => !path.includes('/projects/')) // exclude all projects as they don't have a details page
 }
