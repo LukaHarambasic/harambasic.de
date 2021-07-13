2fba07fda9a2b8507a3a487d1e71c9f0ba166561
LukaHarambasic <luka@harambasic.de>
2021-07-13 22:53:03 +0200

    line break

--- scripts/generate-post/index.js
+++ scripts/generate-post/index.js
@@ -1,5 +1,6 @@
 const { readFileSync, writeFileSync } = require('fs')
 const readline = require('readline')
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
