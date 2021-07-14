d7fee0719c2e817dd15c6c6302102520aac91e5d
LukaHarambasic <luka@harambasic.de>
2021-07-14 22:45:03 +0200

    logging

--- assets/js/getCategoryMeta.js
+++ assets/js/getCategoryMeta.js
@@ -1,6 +1,9 @@
 export default (categories) => {
+  console.log('---------')
   return categories.map((category) => {
+    console.log(category)
     const slug = category.replaceAll(' ', '-').toLowerCase()
+    console.log(slug)
     return {
       title: category,
       slug,
