778f532521623bdc50706af2805c24ec9ce46797
LukaHarambasic <luka@harambasic.de>
2021-07-15 22:00:56 +0200

    refactoring

---
+++ components/Categories/CategoriesDetails.vue
@@ -0,0 +1,109 @@
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="post in posts" :key="post.slug" class="h-feed">
+        <nuxt-link :to="post.path">
+          <div class="meta">
+            <h2 class="title p-name" v-text="post.title" />
+            <time class="date dt-published" :datetime="post.publishedAt">
+              <a :href="post.url" class="u-url">
+                {{ post.publishedAt | date }}
+              </a>
+            </time>
+          </div>
+          <div class="icon">
+            <icons-arrow />
+          </div>
+        </nuxt-link>
+      </li>
+    </ul>
+    <base-footnote>
+      Check out the <a :href="globals.blogFeedURL">RSS feed</a> or
+      <a :href="globals.twitterURL">my Twitter account</a>
+      to keep up to date.
+    </base-footnote>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CategoriesDetails',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+ul
+  display: flex
+  flex-direction: column
+  flex-wrap: nowrap
+  justify-content: flex-start
+  align-content: space-between
+  align-items: stretch
+li
+  > a
+    display: flex
+    flex-direction: row
+    flex-wrap: nowrap
+    justify-content: space-between
+    align-content: flex-start
+    align-items: flex-start
+    text-decoration: none
+    color: var(--c-font)
+    transition: $animation
+    border-radius: $border-radius
+    padding: 2rem
+    &:hover
+      background: var(--c-primary)
+      .icon
+        background: var(--c-surface)
+        svg
+          fill: var(--c-secondary)
+    @media screen and (max-width: $breakpoint-mobile)
+      flex-direction: column
+      justify-content: flex-start
+      align-items: flex-start
+    .icon
+      display: flex
+      flex-direction: row
+      flex-wrap: nowrap
+      justify-content: center
+      align-content: center
+      align-items: center
+      background: var(--c-primary)
+      border-radius: 50%
+      font-size: 1.5rem
+      line-height: 1.5rem
+      padding: .5rem
+      margin:  0 0 0 1rem
+      transition: $animation
+      @media screen and (max-width: $breakpoint-mobile)
+        display: none
+      svg
+        fill: var(--c-font)
+        width: 2rem
+        height: 2rem
+    .title
+      font-size: 1.5rem
+    .date
+      font-size: 1rem
+      @media screen and (max-width: $breakpoint-mobile)
+        margin: .5rem 0 0 0
+      a
+        color: var(--c-font)
+        text-decoration: none
+        transition: $animation
+        border-bottom: 2px solid transparent
+        &:hover
+          border-color: var(--c-font)
+</style>
--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,24 +1,5 @@
 <template>
-  <section>
-    <h2>Categories</h2>
-    <ul>
-      <li v-for="category in categories" :key="category.slug" class="h-feed">
-        <nuxt-link :to="category.path">
-          <div>
-            <h2 class="title p-name" v-text="category.title" />
-          </div>
-          <div class="icon">
-            <icons-arrow />
-          </div>
-        </nuxt-link>
-      </li>
-    </ul>
-    <base-footnote>
-      Check out the <a :href="globals.blogFeedURL">RSS feed</a> or
-      <a :href="globals.twitterURL">my Twitter account</a>
-      to keep up to date.
-    </base-footnote>
-  </section>
+  <categories-overview :categories="categories" />
 </template>
 
 <script>
@@ -30,7 +11,6 @@ export default {
   async asyncData({ $content }) {
     const posts = await $content('posts').sortBy('publishedAt', 'desc').fetch()
     return {
-      posts,
       categories: getCategoriesUniq(posts),
     }
   },
@@ -54,58 +34,3 @@ export default {
   },
 }
 </script>
-
-<style lang="sass" scoped>
-ul
-  display: flex
-  flex-direction: column
-  flex-wrap: nowrap
-  justify-content: flex-start
-  align-content: space-between
-  align-items: stretch
-li
-  > a
-    display: flex
-    flex-direction: row
-    flex-wrap: nowrap
-    justify-content: space-between
-    align-content: flex-start
-    align-items: flex-start
-    text-decoration: none
-    color: var(--c-font)
-    transition: $animation
-    border-radius: $border-radius
-    padding: 2rem
-    &:hover
-      background: var(--c-primary)
-      .icon
-        background: var(--c-surface)
-        svg
-          fill: var(--c-secondary)
-    @media screen and (max-width: $breakpoint-mobile)
-      flex-direction: column
-      justify-content: flex-start
-      align-items: flex-start
-    .icon
-      display: flex
-      flex-direction: row
-      flex-wrap: nowrap
-      justify-content: center
-      align-content: center
-      align-items: center
-      background: var(--c-primary)
-      border-radius: 50%
-      font-size: 1.5rem
-      line-height: 1.5rem
-      padding: .5rem
-      margin:  0 0 0 2rem
-      transition: $animation
-      @media screen and (max-width: $breakpoint-mobile)
-        display: none
-      svg
-        fill: var(--c-secondary)
-        width: 2rem
-        height: 2rem
-    .title
-      font-size: 1.5rem
-</style>
