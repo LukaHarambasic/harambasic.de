402c34b108fc34df16aec95b937cb0d0da4e23f8
LukaHarambasic <luka@harambasic.de>
2021-07-19 21:28:09 +0200

    report error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,14 @@
 <template>
   <section>
-    <h2>An error occurred or page not found</h2>
+    <h2>An error occurred or page not found ({{ error.statusCode }})</h2>
     <div class="nuxt-content">
       <p>
         But why should you care about the error reason? Click on one of the
         links in the navigation or go directly to
         <nuxt-link to="/">home</nuxt-link>. But you won't earn 200€.
+      </p>
+      <p>
+        <a :href="createIssueUrl">Please report this on GitHub.</a> Thanks ❤️
       </p>
     </div>
     <base-footnote>
@@ -22,6 +25,15 @@ export default {
     error: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    createIssueUrl() {
+      console.log(this.$route)
+      const base = `https://github.com/LukaHarambasic/harambasic.de/issues/new`
+      const title = `title=Page%20not%20found:%20${this.$route.fullPath}`
+      const body = `body=The%20following%20page%20is%20currently%20not%20available:%20${this.$route.fullPath}%20(error%20code:%20${this.error.statusCode})`
+      return `${base}?${title}&${body}`
     },
   },
 }
