eaa86daf13b9a8babf925c2d48cd8e2ec47e879a
LukaHarambasic <luka@harambasic.de>
2021-09-20 17:07:17 +0200

    feat(cv): new cv as pdf with adobe sdk

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,47 +1,28 @@
 <template>
-  <client-only>
-    <resume
-      class="cv"
-      :segments="segments"
-      :skills="skills"
-      :information="information"
-      :settings="settings"
-    />
-  </client-only>
+  <div id="inline-pdf-cv" />
 </template>
 
 <script>
-import resume from 'vue-resume-component'
-import segments from '@/content/cv/segments.json'
-import skills from '@/content/cv/skills.json'
-import information from '@/content/cv/information.json'
-import settings from '@/content/cv/settings.json'
 import getSiteMeta from '@/assets/js/getMeta'
 export default {
   name: 'Cv',
-  components: {
-    resume,
-  },
   layout: 'cv',
-  data() {
-    return {
-      segments,
-      skills,
-      information,
-      settings,
-    }
-  },
   head() {
     return {
       title: this.meta.title,
       meta: [...this.meta],
+      script: [
+        {
+          src: 'https://documentcloud.adobe.com/view-sdk/main.js',
+        },
+      ],
     }
   },
   computed: {
     meta() {
       const metaData = {
         title: `CV`,
-        description: 'My personal CV as a website.',
+        description: 'The CV of Luka Harambasic.',
         url: `/cv`,
         img: `/social/cv.png`,
         imgAlt: `CV - ${this.globals.title}`,
@@ -49,7 +30,40 @@ export default {
       return getSiteMeta(metaData)
     },
   },
+  mounted() {
+    this.$nextTick(() => {
+      console.log('nextTick')
+      this.addPdfListener()
+    })
+  },
+  methods: {
+    addPdfListener() {
+      document.addEventListener('adobe_dc_view_sdk.ready', () => {
+        console.log('addEventListener')
+        // eslint-disable-next-line no-undef
+        const adobeDCView = new AdobeDC.View({
+          clientId: '8f3b7a04dc41427db4d6dfb54428b8d6', // TODO process.env.ADOBE_PDF_VIEWER_CLIENT_ID,
+          divId: 'inline-pdf-cv',
+        })
+        adobeDCView.previewFile(
+          {
+            content: { location: { url: '/cv.pdf' } },
+            metaData: { fileName: 'Luka Harambasic - CV' },
+          },
+          {
+            embedMode: 'IN_LINE',
+            showDownloadPDF: false,
+            showPrintPDF: false,
+          }
+        )
+      })
+    },
+  },
 }
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+#inline-pdf-cv
+  width: $size-desktop
+  max-width: 100%
+</style>
