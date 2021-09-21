121a449ddbc5401df092ae765dea72d860e13674
LukaHarambasic <luka@harambasic.de>
2021-09-21 16:07:23 +0200

    feat(cv): forward cv to cv.pdf

---
+++ pages/cv.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>You'll get forwarded...</h2>
+</template>
+
+<script>
+export default {
+  name: 'Cv',
+  mounted() {
+    window.location.replace('https://harambasic.de/cv.pdf')
+  },
+}
+</script>
+
+<style scoped></style>
