b759933ed532d59a222fc28a2a13c1c66f3aeb29
LukaHarambasic <luka@harambasic.de>
2021-10-06 15:11:48 +0200

    fix(header): remove wrongly placed icon

--- components/Layout/TheLayoutHeader.vue
+++ components/Layout/TheLayoutHeader.vue
@@ -8,9 +8,7 @@
         <li><nuxt-link to="/lists">Lists</nuxt-link></li>
         <li><nuxt-link to="/posts">Blog</nuxt-link></li>
         <li>
-          <a href="https://harambasic.de/Luka_Harambasic.pdf">
-            CV<i class="ri-external-link-fill"></i>
-          </a>
+          <a href="https://harambasic.de/Luka_Harambasic.pdf">CV</a>
         </li>
       </ul>
     </nav>
