2c968171095382cfa24f2aaec3a999aadc58d5ad
LukaHarambasic <luka@harambasic.de>
2021-11-29 15:30:01 +0100

    feat(services): add tool logos

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -16,7 +16,7 @@
           <br />
           <span class="subtitle">Consulting</span>
         </h3>
-        <p>
+        <p class="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -29,11 +29,11 @@
       <div class="service">
         <icons-tool class="icon" />
         <h3 class="header">
-          <span class="title">Digital Tool</span>
+          <span class="title">New Work</span>
           <br />
           <span class="subtitle">Consulting</span>
         </h3>
-        <p>
+        <p class="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -43,6 +43,20 @@
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
+    </section>
+    <section class="tools">
+      <ul>
+        <li v-for="logo in logos" :key="logo.path">
+          <a :href="logo.url">
+            <nuxt-picture
+              :src="logo.path"
+              :alt="logo.alt"
+              sizes="xs:200px md:500px"
+            />
+          </a>
+        </li>
+      </ul>
+      <p class="disclaimer">I'm not affiliated with any of the companies.</p>
     </section>
     <section class="cta">
       <a href="/services/book" class="button">Book an appointment!</a>
@@ -55,6 +69,112 @@ import getSiteMeta from '@/assets/js/getMeta'
 
 export default {
   name: 'Services',
+  data() {
+    return {
+      logos: [
+        {
+          path: 'services/airtable.svg',
+          alt: 'Airtable Logo',
+          url: 'https://airtable.com/',
+        },
+        {
+          path: 'services/asana.svg',
+          alt: 'Asana Logo',
+          url: 'https://asana.com/de',
+        },
+        {
+          path: 'services/buffer.svg',
+          alt: 'Buffer Logo',
+          url: 'https://buffer.com/',
+        },
+        {
+          path: 'services/copper.svg',
+          alt: 'Copper Logo',
+          url: 'https://www.copper.com/',
+        },
+        {
+          path: 'services/google_workspace.svg',
+          alt: 'Google Workspace Logo',
+          url: 'https://workspace.google.com/',
+        },
+        {
+          path: 'services/hubspot.svg',
+          alt: 'Hubspot Logo',
+          url: 'https://www.hubspot.com/',
+        },
+        {
+          path: 'services/jira.svg',
+          alt: 'Jira Software Logo',
+          url: 'https://www.atlassian.com/software/jira',
+        },
+        {
+          path: 'services/mailchimp.svg',
+          alt: 'Mailchimp Logo',
+          url: 'https://mailchimp.com/',
+        },
+        {
+          path: 'services/microsoft_365.svg',
+          alt: 'Microsoft 365 Logo',
+          url: 'https://www.microsoft.com/en-us/microsoft-365',
+        },
+        {
+          path: 'services/miro.svg',
+          alt: 'Miro Logo',
+          url: 'https://miro.com/',
+        },
+        {
+          path: 'services/mural.svg',
+          alt: 'Mural Logo',
+          url: 'https://www.mural.co/',
+        },
+        {
+          path: 'services/nextcloud.svg',
+          alt: 'NextCloud Logo',
+          url: 'https://nextcloud.com/',
+        },
+        {
+          path: 'services/notion.svg',
+          alt: 'Notion Logo',
+          url: 'https://www.notion.so/',
+        },
+        {
+          path: 'services/slab.svg',
+          alt: 'Slab Logo',
+          url: 'https://slab.com/',
+        },
+        {
+          path: 'services/slack.svg',
+          alt: 'Slack Logo',
+          url: 'https://slack.com/',
+        },
+        {
+          path: 'services/todoist.svg',
+          alt: 'todoist Logo',
+          url: 'https://todoist.com/',
+        },
+        {
+          path: 'services/toggl.svg',
+          alt: 'Toggl Logo',
+          url: 'https://toggl.com/',
+        },
+        {
+          path: 'services/typeform.svg',
+          alt: 'Typeform Logo',
+          url: 'https://www.typeform.com/',
+        },
+        {
+          path: 'services/zapier.svg',
+          alt: 'Zapier Logo',
+          url: 'https://zapier.com/',
+        },
+        {
+          path: 'services/zoom.svg',
+          alt: 'Zoom Logo',
+          url: 'https://zoom.us/',
+        },
+      ],
+    }
+  },
   head() {
     return {
       title: this.meta.title,
@@ -78,6 +198,9 @@ export default {
 section
   margin: 0 0 6rem 0
 .services
+  width: $size-desktop-breakout
+  position: relative
+  left: calc(calc($size-desktop-breakout / -2) + 50%)
   display: flex
   flex-direction: row
   flex-wrap: nowrap
@@ -89,7 +212,7 @@ section
   .service
     text-align: center
     background: var(--c-light)
-    padding: 2rem
+    padding: 4rem
     border-radius: var(--border-radius)
     &:first-of-type
       margin: 0 4rem 0 0
@@ -112,8 +235,32 @@ section
         color: var(--c-font-on-primary)
         padding: .25rem .5rem
         font-weight: normal
-    p
+        border: 2rem
+    .description
       text-align: left
+      //border-bottom: 1px solid var(--c-surface)
+      //padding: 0 0 2rem 0
+      //margin: 0 0 2rem 0
+.tools
+  ul
+    display: flex
+    flex-direction: row
+    flex-wrap: wrap
+    justify-content: center
+    align-content: flex-start
+    align-items: flex-start
+    gap: 2rem
+    margin: 0 0 2rem 0
+    li
+      a
+        filter: grayscale(100%)
+        transition: var(--transition)
+        &:hover
+          filter: grayscale(0)
+  .disclaimer
+    font-size: 0.9rem
+    opacity: 0.9
+    text-align: center
 .cta
   text-align: center
   .button
