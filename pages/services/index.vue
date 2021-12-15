feaabfa54d015274da96e0351f24a8f89d1cd8e6
Luka Harambasic <luka@harambasic.de>
2021-12-15 14:39:40 +0100

    need to trigger a build

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -4,7 +4,7 @@
       <h2>Services</h2>
       <p>
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </section>
