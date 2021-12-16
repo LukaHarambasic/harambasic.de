8f4a6d736b1223e5473b9da3e6be58fd64974d5a
LukaHarambasic <luka@harambasic.de>
2021-12-16 01:24:35 +0100

    feat(services): extract services to json

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -47,6 +47,7 @@
 import ServicesTestimonials from '@/components/Services/ServicesTestimonials'
 import getSiteMeta from '@/assets/js/getMeta'
 import { logos } from '@/content/services/logos'
+import { services } from '@/content/services/services'
 
 export default {
   name: 'Services',
@@ -56,29 +57,11 @@ export default {
   data() {
     return {
       logos,
-      services: [
-        {
-          title: 'Agile Project<br />Management',
-          icon: 'Agile',
-          description:
-            'Software teams need team leads with technical and deep methodically knowledge, like Scrum and Kanban. Therefore, I can take on the role of a Scrum Master and Product Owner to build bridges between business, IT, and external stakeholders.',
-        },
-        {
-          title: 'New Work<br />Consulting',
-          icon: 'Tool',
-          description:
-            'Digital Transformations are happening everywhere, which changes how organizations function, teams communicate, and individuals work. These restructurings lead to process optimization and automation. I uncover the potential with Design thinking Workshops.',
-        },
-        {
-          title: 'Technical<br />Partnership',
-          icon: 'Tech',
-          description:
-            'New concepts sometimes need help to realize them. If the industry knowledge exists but lacks the technical expertise, external support is required. To answer questions about the architecture, technologies, platforms, and whom to hire.',
-        },
-      ],
+      services,
     }
   },
   head() {
+    console.log(this.meta.title)
     return {
       title: this.meta.title,
       meta: [...this.meta],
