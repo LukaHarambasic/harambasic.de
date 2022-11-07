b88f7e23510fc53f8e8bfb71b2fa9e1ed9484bf5
LukaHarambasic <luka@harambasic.de>
2022-11-07 23:46:46 +0100

    added toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import astroRemark from '@astrojs/markdown-remark';
+
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -6,5 +8,15 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   // Enable Vue to support Vue components.
-  integrations: [, image()]
+  integrations: [, image()],
+  markdown: {
+    rehypePlugins: [
+      'rehype-slug',
+      ['rehype-autolink-headings', { behavior: 'append' }],
+      ['rehype-toc', { headings: ['h1', 'h2'] }]
+    ],
+    // Preserve Astro's default plugins: GitHub-flavored Markdown and Smartypants
+    // default: false
+    extendDefaultPlugins: true,
+  },
 });
