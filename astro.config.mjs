e490fa44ab46a228807d022c1e4feda817aa5c30
LukaHarambasic <luka@harambasic.de>
2022-11-14 16:47:50 +0100

    Clipboard button with svelete

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,18 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
-	// Enable Vue to support Vue components.
-	integrations: [, image()],
-	markdown: {
-		rehypePlugins: [
-			'rehype-slug',
-			['rehype-autolink-headings', { behavior: 'append' }],
-		],
-		// Preserve Astro's default plugins: GitHub-flavored Markdown and Smartypants
-		// default: false
-		extendDefaultPlugins: true,
-	},
+  // Enable Vue to support Vue components.
+  integrations: [, image(), svelte()],
+  markdown: {
+    rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+      behavior: 'append'
+    }]],
+    // Preserve Astro's default plugins: GitHub-flavored Markdown and Smartypants
+    // default: false
+    extendDefaultPlugins: true
+  }
 });
---
+++ src/components/Base/BaseToClipboardButton.svelte
@@ -0,0 +1,34 @@
+<script>
+	// TODO typesript
+	// TODO prettier for svelte
+	export let toClipboard;
+	function copyToClipboard() {
+		navigator.clipboard.writeText(toClipboard);
+	}
+</script>
+
+<button class="button" on:click|once={copyToClipboard}>
+	<slot />
+</button>
+
+<style>
+	.button {
+		border: none;
+		background: transparent;
+		font-weight: 500;
+		font-size: 1rem;
+		font-family: 'Source Sans Pro', sans-serif;
+		letter-spacing: 0.2px;
+		line-height: 1.2;
+		border-right: 1px solid rgba(0, 0, 0, 0.05);
+		padding: var(--xs) var(--s);
+		margin: 0;
+		transition: var(--transition);
+		text-decoration: none;
+		color: var(--c-font);
+		&:hover {
+			background: rgba(0, 0, 0, 0.05);
+			cursor: pointer;
+		}
+	}
+</style>
