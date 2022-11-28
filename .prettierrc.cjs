0ed4fb2f7a4ad7c1bfdd0c70a4c12b3aa442dfce
Luka Harambasic <luka@harambasic.de>
2022-11-28 17:10:38 +0100

    works again

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
   tabWidth: 2,
   trailingComma: 'es5',
   useTabs: true,
-  organizeImportsSkipDestructiveCodeActions: true,
   plugins: ['./node_modules/prettier-plugin-astro', './node_modules/prettier-plugin-svelte'],
   overrides: [
     {
