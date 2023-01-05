2090c45906076ce534909d80d52b5e0264cf64ec
Luka Harambasic <luka@harambasic.de>
2023-01-05 21:04:23 +0100

    it is still not working

--- src/lib/util/markdown.ts
+++ src/lib/util/markdown.ts
@@ -1,11 +1,11 @@
-import type { EntryType } from '$lib/types/enums';
+import { EntryType } from '$lib/types/enums';
 import { join } from 'path';
 import * as fs from 'fs/promises';
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
-import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
+// import remarkStringify from 'remark-stringify'
 import remarkFrontmatter from 'remark-frontmatter';
 
 export async function getEntries(entryType: EntryType): Promise<string[]> {
@@ -14,18 +14,30 @@ export async function getEntries(entryType: EntryType): Promise<string[]> {
     return await Promise.all(
         fileNames.map(async (fileName) => {
             const filePath = join(folderPath, fileName);
-            return await fs.readFile(filePath, { encoding: 'utf8' });
+            const file: string = await fs.readFile(filePath, 'utf8');
+            return getMarkdown(file)
         })
     );
 }
 
 // TODO type: satisfies entry?
-export async function getMarkdown(raw: string) {
-    return await unified()
+async function getMarkdown(raw: string): Promise<string> {
+    const result = await unified()
+        .use(remarkFrontmatter, ['yaml'])
         .use(remarkParse)
         .use(remarkRehype)
-        .use(remarkFrontmatter)
-        .use(rehypeSanitize)
         .use(rehypeStringify)
         .process(raw)
+    return result.toString()
 }
+
+// import fs from 'fs';
+// import { unified } from 'unified'
+// import markdown from 'remark-parse';
+// import frontmatter from 'remark-frontmatter';
+// import remark2rehype from 'remark-rehype';
+// import rehype2html from 'rehype-stringify';
+//         .use(markdown)
+//     .use(frontmatter)
+//     .use(remark2rehype)
+//     .use(rehype2html)
