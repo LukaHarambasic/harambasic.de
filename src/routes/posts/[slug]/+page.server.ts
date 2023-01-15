52fbdd68a95bf438dfe84efce31df8d12123b7a0
Henry Stoll <a@henrystoll.de>
2023-01-15 11:23:12 +0000

    it works

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -1,10 +1,18 @@
 import type { PageServerLoad } from './$types'
 
-export const load = (async () => {
+export const load = (async ({ parent, params }) => {
     //TODO other meta data
     // TODO
+    // console.log('args', args);
+    const {posts, projects}  = await parent();
+    const [
+        entries,
+        tags
+      ] = posts
+    const entry = entries.find((post) => post.slug === params.slug);
     return {
-        title: 'Static post for testing',
-        // entry: await getEntryBySlug('add-github-actions-for-testing-linting-to-your-repository')
+        title: entry.title,
+        entry
     }
 }) satisfies PageServerLoad
+
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import type { Post } from '$lib/types/post'
   import type { PageData } from './$types'
 
   export let data: PageData
-  const entry = data.posts[0]
+  const entry = data.entry
 </script>
 
-{entry.title}
+{entry?.title}
