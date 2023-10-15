762767a7893043bfbf02f67c9481a460151d9818
Luka Harambasic <luka@harambasic.de>
2023-10-16 00:02:02 +0200

    testing env

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -12,7 +12,8 @@ export const load = (async ({url}) => {
     title: 'Projects',
     description: "Check out all the projects I've worked on over the years! Get a glimpse of my diverse range of skills and interests.",
     socialImgAlt: 'TODO',
-    relativePath: pathname,
+    relativePath: url.pathname,
+    href: url.href,
     permalink: env.DEPLOY_PRIME_URL || env.URL,
   }
 }) satisfies PageServerLoad
