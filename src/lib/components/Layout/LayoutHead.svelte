cd8839399a7d86bd85a50d4a14287952c2205d73
Luka Harambasic <luka@harambasic.de>
2023-10-30 19:39:01 +0100

    another one

--- src/lib/components/Layout/LayoutHead.svelte
+++ src/lib/components/Layout/LayoutHead.svelte
@@ -16,6 +16,7 @@
   console.log(getSlug(title))
   console.log(permalink)
   console.log(relativePath)
+  console.log(`${permalink}/social/${getSlug(title)}.png`)
   $: socialImg = `${permalink}/social/${getSlug(title)}.png` || `${permalink}/social/default.png`
 </script>
 
