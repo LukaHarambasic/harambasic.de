3a20a7ac076b4394e5b0f0984af4ab0254e46a3d
Luka Harambasic <luka@harambasic.de>
2023-11-16 22:36:37 +0100

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,7 @@
   <div class="inner">
     <img src="../profile.jpeg" alt="Profile of Luka Harambasic" class="profile" />
     <div class="content rich-text">
-      <h2>Heyho, I'm Luka!</h2>
+      <h2>Heyho, I'm Leo!</h2>
       <p>
         I'm a German/Croatian, based in the beautiful Copenhagen (Denmark). Wrapping up my master in <a href="https://www.entrepreneurship.dtu.dk/education/msc-technology-entrepreneurship">Technology Entrepreneurship at DTU</a>. At <a href="https://monta.com/">Monta</a>, I work as a Product Manger. Feel free to explore my past <a href="/projects">projects</a> or check out my <a href="https://www.linkedin.com/in/harambasic/">LinkedIn profile</a>. If you want to <a href="#contact">chat</a>, just start a conversation about handball, woodworking, cooking, sustianability, or dive into product and IT topics.
       </p>
