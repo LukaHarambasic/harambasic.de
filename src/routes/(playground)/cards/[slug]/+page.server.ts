8b0b39a9f3e47c542d96c9216dc35af18a825203
Luka Harambasic <luka@harambasic.de>
2023-12-26 13:01:25 +0100

    maybe ?

--- src/routes/(playground)/cards/[slug]/+page.server.ts
+++ src/routes/(playground)/cards/[slug]/+page.server.ts
@@ -1,11 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
-
+import type { EntryGenerator, RouteParams } from './$types';
+import { s } from 'vitest/dist/reporters-5f784f42';
 const supabase = createClient(
   'https://xqlghnitokncvzvxoiyq.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxbGdobml0b2tuY3Z6dnhvaXlxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzQyMzYwNSwiZXhwIjoxOTk4OTk5NjA1fQ.QzoaOpgNPQF32zJl5c-Olx6ZEhw03M5mkbAA_zkBrT8'
 )
 
 export interface Card {
+    slug: string,
     name: string,
     content: string,
     language: string,
@@ -13,21 +15,41 @@ export interface Card {
 
 export const prerender = true
 
-async function fetchCard(slug: string): Promise<Card> {
-    const { data, error } = await supabase.from('cards').select('*').eq('uuid', slug)
+async function fetchCards(): Promise<Card[]> {
+    const { data, error } = await supabase.from('cards').select('*')
     if (error) {
         throw new Error(`error`)
     }
     if (!data) {
         throw new Error('No data: data')
     }
+    return data.map(({uuid, name, content, language}) => {
+        return {
+            slug: uuid,
+            name: name,
+            content: content,
+            language: language,
+        }
+    })
+}
+
+function getCardBySlug(slug: string, cards: Card[]): Card | undefined {
+    return cards.find((card) => card.name === slug)
+}
+
+function getEntries(cards: Card[]): RouteParams[] {
+    return cards.map(({slug}) => ({ slug }))
+}
+
+export const load = async ({params}) => {
+    const cards = await fetchCards()
     return {
-        name: data[0].name,
-        content: data[0].content,
-        language: data[0].language,
+        card: await getCardBySlug(params.slug, cards)
     }
 }
 
-export const load = async ({params}) => ({  
-    card: await fetchCard(params.slug)
-})
+export const entries: EntryGenerator = async () => {
+    const cards = await fetchCards()
+    console.log(getEntries(cards))
+	return getEntries(cards)
+};
