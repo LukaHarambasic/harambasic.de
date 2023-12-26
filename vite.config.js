077eaef5bc5c7f6dbd96ab9f17ad616ead79779d
Luka Harambasic <luka@harambasic.de>
2023-12-26 13:07:21 +0100

    another one

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
-import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  resolve: {
-    alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-    },
-  },
   plugins: [enhancedImages(), sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
