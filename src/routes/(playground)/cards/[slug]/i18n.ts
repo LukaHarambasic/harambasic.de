d5d4c9035860fc93689a1b1e35aa6c5224e8b8f6
Luka Harambasic <luka@harambasic.de>
2023-12-26 23:43:56 +0100

    more

--- src/routes/(playground)/cards/[slug]/i18n.ts
+++ src/routes/(playground)/cards/[slug]/i18n.ts
@@ -1,10 +1,8 @@
 export function t(key: string, languageCode: string, value?: any): string {
     const languages = import.meta.glob('./i18n.*.ts', { eager: true })
-
     const languageModule = languages[`./i18n.${languageCode}.ts`]
-
     if (!languageModule) {
-        return key
+        return `error.${key}`
     }
     const language = languageModule.default
     return value ? language[key](value) : language[key]
