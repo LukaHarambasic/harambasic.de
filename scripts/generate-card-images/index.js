05d60cfbcca8b4701fe5a16a0ec4186ad114d6b8
Luka Harambasic <luka@harambasic.de>
2023-12-28 00:09:38 +0100

    ahhhh languages are missing

--- scripts/generate-card-images/index.js
+++ scripts/generate-card-images/index.js
@@ -6,6 +6,7 @@ const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY)
 
 async function _fetchCards() {
+    console.log('Fetching cards...')
     const { data, error } = await supabase.from('cards').select('*').is('image_url', null);
     console.log('Cards fetched: ', data.length)
     if (error) throw new Error(`error`)
@@ -23,6 +24,7 @@ async function _fetchCards() {
 // TODO types could be added to the table and based on that a different prompt can be selected, e.g. if the type is christmas the picture below gets generated
 // ok than also the text needs to be changed, but maybe as a type in the translation file as well?
 async function _generateImage(name) {
+    console.log('Generating image...')
     const image = await openai.images.generate({
         model: "dall-e-3",
         prompt: `abstract layered christmas themed with light refracting , very cool subtle minimal christmas illustration, christmas red  with subtle highlights. each image is unique for a user, use their name in some way to make it more personal, it doesnt has to be integrated directly: ${name}`,
@@ -33,24 +35,37 @@ async function _generateImage(name) {
 }
 
 async function _fetchImage(imageUrl) {
+    console.log('Fetching image...')
     const response = await fetch(imageUrl);
     if (!response.ok) throw new Error('Failed to fetch image');
     return await response.buffer();
 }
 
 async function _saveImage(blob, uuid) {
+    console.log('Uploading image...')
     const { data, error } = await supabase.storage.from('cards').upload(`${uuid}.jpg`, blob);
     if (error) throw new Error('Failed to upload image: ', error);
     const imageUrl = `${process.env.SUPABASE_URL}/storage/v1/object/public/${data.fullPath}`
     await supabase.from('cards').update({image_url: imageUrl}).eq('uuid', uuid);
 }
 
+async function _triggerBuild() {
+    console.log('Triggering build...')
+    const response = await fetch(process.env.NETLIFY_BUILD_HOOK, {
+        method: 'POST',
+    });
+    if (!response.ok) throw new Error('Failed to trigger build');
+}
+
 async function main() {
     const cards = await _fetchCards()
     cards.forEach(async (card) => {
+        console.log(`Generating image for card: ${card.name}`)
         const imageUrl = await _generateImage(card.name)
         const blob = await _fetchImage(imageUrl)
         await _saveImage(blob, card.uuid)
+        await _triggerBuild()
+        console.log('Done!')
     })
 }
 
---
+++ src/routes/(playground)/cards/[slug]/i18n.fr.ts
@@ -0,0 +1,13 @@
+export default {
+    'title': (value: string) => `Courrier pour ${value}`,
+    'description': () => `Luka vous a envoyé une carte postale numérique.`,
+    'socialImgAlt': () => 'Image de carte postale purement décorative',
+    'greeting': (value: string) => `Heyho ${value},`,
+    'farewell': () => 'Joyeux Noël, ',
+    'frontTitle': (value: string) => `Joyeux Noël ${value}`,
+    'frontGenerated': (value: string) => `Image unique générée pour <em>${value}</em>`,
+    'frontBlink': () => `Cliquez ici !`,
+    'footerGenerated': (value: string) => `Image unique générée pour <em>${value}</em> par OpenAi - DALL·E 3`,
+    'footerBy': () => `Conçu & développé par <a style="color: rgba(1, 3, 15, 0.6);" href="https://harambasic.de">Luka Harambasic</a>`,
+    'backDownload': () => `Télécharger l'image`,
+}
