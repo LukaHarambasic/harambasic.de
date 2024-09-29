807d0dc5826e11ce8d64e8711501cab2ee1795ce
Luka Harambasic <luka@harambasic.de>
2024-09-29 17:44:48 +0200

    astro js is comming

--- src/routes/(main)/data-privacy/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-	return {
-		title: 'Data Privacy',
-		description: 'Data Privacy, not more, but also not less.'
-	};
-}) satisfies PageServerLoad;
