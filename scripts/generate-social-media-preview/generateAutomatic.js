ff525723cbca9a68e19aaa344848f182f57a120f
Luka Harambasic <luka@harambasic.de>
2024-09-30 17:25:51 +0200

    format & eslint

--- scripts/generate-social-media-preview/generateAutomatic.js
+++ scripts/generate-social-media-preview/generateAutomatic.js
@@ -1,51 +1,61 @@
-import { readFileSync, readdirSync } from 'fs';
-import fm from 'front-matter';
-import yaml from 'js-yaml';
-import { chromium } from 'playwright';
-import { ROOT_PATH, doesImageAlreadyExist, fileToMeta, generateImage } from './util.js';
+import { readFileSync, readdirSync } from 'fs'
+import fm from 'front-matter'
+import yaml from 'js-yaml'
+import { chromium } from 'playwright'
+import {
+    ROOT_PATH,
+    doesImageAlreadyExist,
+    fileToMeta,
+    generateImage,
+} from './util.js'
 
-const POSTS_PATH = `${ROOT_PATH}/src/content/posts`;
-const LISTS_PATH = `${ROOT_PATH}/src/content/uses`;
+const POSTS_PATH = `${ROOT_PATH}/src/content/posts`
+const LISTS_PATH = `${ROOT_PATH}/src/content/uses`
 
 export function getSlug(str) {
-	if (!str) return '';
-	const slug = str
-		.trim()
-		.toLowerCase()
-		// remove all chars which aren't characters, numbers or spaces
-		.replace(/[^a-zA-Z0-9\s]+/g, '')
-		// replace all spaces with dashes
-		.replace(/\s+/g, '-');
-	return slug;
+    if (!str) return ''
+    const slug = str
+        .trim()
+        .toLowerCase()
+        // remove all chars which aren't characters, numbers or spaces
+        .replace(/[^a-zA-Z0-9\s]+/g, '')
+        // replace all spaces with dashes
+        .replace(/\s+/g, '-')
+    return slug
 }
 
 const generateSocialMediaPreview = async () => {
-	console.log('>> GENERATE SOCIAL MEDIA PREVIEWS <<');
-	console.log('🆕 newly generated, 🛑 already exists');
-	console.log('-------------------------------------');
-	const browser = await chromium.launch();
-	const page = await browser.newPage();
-	const posts = readdirSync(POSTS_PATH).map((name) => fileToMeta(name, POSTS_PATH));
-	const lists = readdirSync(LISTS_PATH).map((name) => fileToMeta(name, LISTS_PATH));
-	const files = [...posts, ...lists];
-	for (const file of files) {
-		const data = readFileSync(file.path, 'utf8');
-		const content = file.type === 'md' ? fm(data) : yaml.load(data);
-		const title = file.type === 'md' ? content.attributes.title : content.title;
-		if (!doesImageAlreadyExist(file.slug)) {
-			console.log('🆕 Post/List:', title);
-			await generateImage(page, title, file.slug);
-		} else {
-			console.log('🛑 Post/List:', title);
-		}
-	}
-	await browser.close();
-};
+    console.log('>> GENERATE SOCIAL MEDIA PREVIEWS <<')
+    console.log('🆕 newly generated, 🛑 already exists')
+    console.log('-------------------------------------')
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    const posts = readdirSync(POSTS_PATH).map((name) =>
+        fileToMeta(name, POSTS_PATH)
+    )
+    const lists = readdirSync(LISTS_PATH).map((name) =>
+        fileToMeta(name, LISTS_PATH)
+    )
+    const files = [...posts, ...lists]
+    for (const file of files) {
+        const data = readFileSync(file.path, 'utf8')
+        const content = file.type === 'md' ? fm(data) : yaml.load(data)
+        const title =
+            file.type === 'md' ? content.attributes.title : content.title
+        if (!doesImageAlreadyExist(file.slug)) {
+            console.log('🆕 Post/List:', title)
+            await generateImage(page, title, file.slug)
+        } else {
+            console.log('🛑 Post/List:', title)
+        }
+    }
+    await browser.close()
+}
 
-(async () => {
-	try {
-		await generateSocialMediaPreview();
-	} catch (error) {
-		console.log('Error:', error);
-	}
-})();
+;(async () => {
+    try {
+        await generateSocialMediaPreview()
+    } catch (error) {
+        console.log('Error:', error)
+    }
+})()
--- scripts/generate-social-media-preview/util.js
+++ scripts/generate-social-media-preview/util.js
@@ -6,38 +6,38 @@
  * https://stackoverflow.com/questions/48301280/how-to-manually-run-a-git-pre-commit-hook-without-attempting-a-commit
  */
 
-import path from 'path';
-import { readdirSync } from 'fs';
+import path from 'path'
+import { readdirSync } from 'fs'
 
-export const ROOT_PATH = process.cwd();
-export const SOCIAL_PATH = `${ROOT_PATH}/static/social`;
+export const ROOT_PATH = process.cwd()
+export const SOCIAL_PATH = `${ROOT_PATH}/static/social`
 
 export const doesImageAlreadyExist = (slug) => {
-	const files = readdirSync(SOCIAL_PATH);
-	return files.find((file) => file.startsWith(slug));
-};
+    const files = readdirSync(SOCIAL_PATH)
+    return files.find((file) => file.startsWith(slug))
+}
 
 export const generateImage = async (page, title, slug) => {
-	const URL = `file:///${path.join(
-		ROOT_PATH,
-		'/scripts/generate-social-media-preview/template.html'
-	)}`;
-	const SCREENSHOT_PATH = `${SOCIAL_PATH}/${slug}.png`;
-	await page.goto(URL);
-	// strange syntax, check https://playwright.dev/docs/api/class-page#page-eval-on-selector for more infos
-	await page.$eval('.title', (el, title) => (el.textContent = title), title);
-	const cardHandle = await page.$('.container');
-	await cardHandle.screenshot({
-		type: 'png',
-		path: SCREENSHOT_PATH
-	});
-};
+    const URL = `file:///${path.join(
+        ROOT_PATH,
+        '/scripts/generate-social-media-preview/template.html'
+    )}`
+    const SCREENSHOT_PATH = `${SOCIAL_PATH}/${slug}.png`
+    await page.goto(URL)
+    // strange syntax, check https://playwright.dev/docs/api/class-page#page-eval-on-selector for more infos
+    await page.$eval('.title', (el, title) => (el.textContent = title), title)
+    const cardHandle = await page.$('.container')
+    await cardHandle.screenshot({
+        type: 'png',
+        path: SCREENSHOT_PATH,
+    })
+}
 
 export const fileToMeta = (name, basePath) => {
-	return {
-		name,
-		path: `${basePath}/${name}`,
-		slug: name.split('.')[0],
-		type: name.split('.')[1]
-	};
-};
+    return {
+        name,
+        path: `${basePath}/${name}`,
+        slug: name.split('.')[0],
+        type: name.split('.')[1],
+    }
+}
