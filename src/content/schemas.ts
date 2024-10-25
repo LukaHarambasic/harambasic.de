d050c8ac143b54f71fc7d8ea8a9db4835d70c8b8
Luka Harambasic <luka@harambasic.de>
2024-10-25 21:07:37 +0200

    format

--- src/content/schemas.ts
+++ src/content/schemas.ts
@@ -1,50 +1,53 @@
 import { reference, z } from 'astro:content'
 
-export const postSchema = ({ image }) => z.object({
-  title: z.string(),
-  description: z.string(),
-  image: image().optional(),
-  published: z.coerce.date(),
-  updated: z.coerce.date(),
-  tags: z.array(reference('tags')),
-  tldr: z.string().optional(),
-  discussion: z.string().url().optional(),
-  category: reference('categories'),
-})
+export const postSchema = ({ image }) =>
+  z.object({
+    title: z.string(),
+    description: z.string(),
+    image: image().optional(),
+    published: z.coerce.date(),
+    updated: z.coerce.date(),
+    tags: z.array(reference('tags')),
+    tldr: z.string().optional(),
+    discussion: z.string().url().optional(),
+    category: reference('categories'),
+  })
 
-export const projectsSchema = ({ image }) => z.object({
-  title: z.string(),
-  description: z.string(),
-  image: image(),
-  imageAlt: z.string(),
-  published: z.coerce.date(),
-  updated: z.coerce.date(),
-  prio: z.number(),
-  status: z.enum(['ACTIVE', 'INACTIVE', 'ARCHIVED']),
-  tags: z.array(reference('tags')),
-  links: z
-    .array(
-      z.object({
-        title: z.string(),
-        url: z.string().url(),
-      })
-    )
-    .optional(),
-  category: reference('categories'),
-})
+export const projectsSchema = ({ image }) =>
+  z.object({
+    title: z.string(),
+    description: z.string(),
+    image: image(),
+    imageAlt: z.string(),
+    published: z.coerce.date(),
+    updated: z.coerce.date(),
+    prio: z.number(),
+    status: z.enum(['ACTIVE', 'INACTIVE', 'ARCHIVED']),
+    tags: z.array(reference('tags')),
+    links: z
+      .array(
+        z.object({
+          title: z.string(),
+          url: z.string().url(),
+        })
+      )
+      .optional(),
+    category: reference('categories'),
+  })
 
-export const usesSchema = ({ image }) => z.object({
-  title: z.string(),
-  description: z.string(),
-  image: image().optional(),
-  published: z.coerce.date(),
-  updated: z.coerce.date(),
-  tags: z.array(reference('tags')),
-  url: z.string().url(),
-  status: z.enum(['ACTIVE', 'INACTIVE', 'ARCHIVED']),
-  openSource: z.boolean().optional(),
-  category: reference('categories'),
-})
+export const usesSchema = ({ image }) =>
+  z.object({
+    title: z.string(),
+    description: z.string(),
+    image: image().optional(),
+    published: z.coerce.date(),
+    updated: z.coerce.date(),
+    tags: z.array(reference('tags')),
+    url: z.string().url(),
+    status: z.enum(['ACTIVE', 'INACTIVE', 'ARCHIVED']),
+    openSource: z.boolean().optional(),
+    category: reference('categories'),
+  })
 
 export const tagsSchema = z.object({
   name: z.string(),
