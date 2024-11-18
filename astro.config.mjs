aa02bd91c05c48ba2856051837cb6268a7f05b71
Luka Harambasic <luka@harambasic.de>
2024-11-18 23:33:13 +0100

    icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import icon from 'astro-icon'
-
-import svelte from '@astrojs/svelte';
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
